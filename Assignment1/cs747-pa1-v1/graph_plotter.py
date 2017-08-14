deb319eb0c00dde87a3722b6c318784f408dc589
Arka Sadhu <ark.sadhu2904@gmail.com>
2017-08-14 21:08:23 +0530

    Fila assgn1 completed

--- Assignment1/cs747-pa1-v1/graph_plotter.py
+++ Assignment1/cs747-pa1-v1/graph_plotter.py
@@ -51,14 +51,23 @@ def plot_algo(algo):
 #     pickle.dump(r4, f)
 
 # plt.plot(np.log10(x1), r1, '-r', np.log10(x2), r2, '-g')
-with open('./eval/ins2_eps.pkl') as f:
+with open('./eval/ins1_eps.pkl') as f:
     r1 = pickle.load(f)
-with open('./eval/ins2_ucb.pkl') as f:
+with open('./eval/ins1_ucb.pkl') as f:
     r2 = pickle.load(f)
-with open('./eval/ins2_klucb.pkl') as f:
+with open('./eval/ins1_klucb.pkl') as f:
     r3 = pickle.load(f)
-with open('./eval/ins2_ths.pkl') as f:
+with open('./eval/ins1_ths.pkl') as f:
     r4 = pickle.load(f)
+# with open('./eval/ins2_eps.pkl') as f:
+#     r1 = pickle.load(f)
+# with open('./eval/ins2_ucb.pkl') as f:
+#     r2 = pickle.load(f)
+# with open('./eval/ins2_klucb.pkl') as f:
+#     r3 = pickle.load(f)
+# with open('./eval/ins2_ths.pkl') as f:
+#     r4 = pickle.load(f)
+
 # plt.plot(x1, r1, '-r', x2, r2, '-g', x3, r3, '-b', x4, r4, '-y')
 x1 = np.arange(1, len(r1) + 1)
 x2 = np.log10(x1)
@@ -72,4 +81,5 @@ plt.semilogx(x1, r3, '-b', label='KL-UCB')
 plt.semilogx(x1, r4, '-y', label='Thompson Sampling')
 plt.legend(bbox_to_anchor=(0.05, 1), loc=2, borderaxespad=0.)
 # plt.show()
-plt.savefig('Instance-25.png')
+# plt.savefig('Instance-25.png')
+# plt.savefig('Instance-5_linear.png')
