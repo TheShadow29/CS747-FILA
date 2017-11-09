f88115978ee7b5108c22231a8163cd518df886c1
Arka Sadhu <ark.sadhu2904@gmail.com>
2017-11-09 20:13:28 +0530

    I think part 2 is done as well, time to write the report

--- Assignment4/code/main_code.py
+++ Assignment4/code/main_code.py
@@ -63,13 +63,13 @@ class mdp_baird(object):
 
 
 class td_lambda(object):
-    def __init__(self, mdp, N, exp_type=1, lamb=0):
+    def __init__(self, mdp, N, lamb=0):
         self.mdp = mdp
         self.lr = 0.001
         self.N = N
         self.lamb = lamb
         self.gamma = self.mdp.gamma
-        self.exp_type = exp_type
+        # self.exp_type = exp_type
         self.str_f = '{:.6} {:.6} {:.6} {:.6} {:.6} {:.6}\n'
 
     def train_model(self):
@@ -84,7 +84,7 @@ class td_lambda(object):
                 next_state = self.mdp.get_next_state(curr_state)
                 vf_curr = self.mdp.get_out_val_fn(curr_state)
                 vf_next = self.mdp.get_out_val_fn(next_state)
-                delta = self.gamma * (vf_next - vf_curr)
+                delta = self.gamma * vf_next - vf_curr
                 tmp1 = self.gamma * self.lamb
                 tmp2 = np.dot(elig_trace, fv_curr_state)
                 tmp3 = (1 - self.lr * self.gamma * self.lamb * tmp2)
@@ -109,7 +109,7 @@ class td_lambda(object):
                 next_state = self.mdp.get_next_state(curr_state)
                 vf_curr = self.mdp.get_out_val_fn(curr_state)
                 vf_next = self.mdp.get_out_val_fn(next_state)
-                delta = self.gamma * (vf_next - vf_curr)
+                delta = self.gamma * vf_next - vf_curr
                 self.mdp.weights = self.mdp.weights + self.lr * delta * fv_curr_state
                 # vf_prev = vf_curr
                 curr_state = next_state
@@ -147,6 +147,7 @@ class td_lambda(object):
     def train_model2(self):
         tit = 0
         ep_no = 0
+        str_to_write = ''
         while tit < self.N:
             curr_state = self.mdp.get_init_state()
             fv_curr_state = self.mdp.get_feat_vec(curr_state)
@@ -158,13 +159,15 @@ class td_lambda(object):
                 next_state = self.mdp.get_next_state(curr_state)
                 vf_curr = self.mdp.get_out_val_fn(curr_state)
                 vf_next = self.mdp.get_out_val_fn(next_state)
-                delta = self.gamma * (vf_next - vf_curr)
+                delta = self.gamma * vf_next - vf_curr
                 tmp1 = self.gamma * self.lamb
                 tmp2 = np.dot(elig_trace, fv_curr_state)
                 tmp3 = (1 - self.lr * self.gamma * self.lamb * tmp2)
                 elig_trace = tmp1 * elig_trace + tmp3 * fv_curr_state
                 self.mdp.weights = (self.mdp.weights + self.lr * (delta + vf_curr - vf_prev) *
                                     elig_trace - self.lr * (vf_curr - vf_prev) * fv_curr_state)
+                vf = self.mdp.get_whole_val_fn()
+                str_to_write += self.str_f.format(*vf)
                 vf_prev = vf_curr
                 curr_state = next_state
                 fv_curr_state = self.mdp.get_feat_vec(next_state)
@@ -173,26 +176,62 @@ class td_lambda(object):
             ep_no += 1
             print('ep_no', ep_no, 'tot_num_iterations', tit)
 
+        str_lamb = str(float(self.lamb))
+        fname = 'exp2_' + str_lamb[0] + '_' + str_lamb[-1] + '.txt'
+        with open(fname, 'w') as f:
+            f.write(str_to_write)
+
 
 def graph_plot():
     with open('./exp1.txt') as f:
         lines = f.read()
 
-    pat = re.compile(r'([-\d.+e]*)\s([-\d.+e]*)\s([-\d.+e]*)\s([-\d.+e]*)\s([-\d.+e]*)\s([-\d.+e]*)\n')
     vf_array1 = np.array(pat.findall(lines)).astype(np.float)
     x_ax = np.arange(vf_array1.shape[0])
-    vf_array2 = np.zeros(vf_array1.shape)
-    vf_array2[vf_array1 < -1] = -np.log10(-vf_array1[vf_array1 < -1])
-    vf_array2[vf_array1 > 1] = np.log10(vf_array1[vf_array1 > 1])
-    vf_array2[np.abs(vf_array1) < 1] = 0
+    # vf_array2 = np.zeros(vf_array1.shape)
+    # vf_array2[vf_array1 < -1] = -np.log10(-vf_array1[vf_array1 < -1])
+    # vf_array2[vf_array1 > 1] = np.log10(vf_array1[vf_array1 > 1])
+    # vf_array2[np.abs(vf_array1) < 1] = 0
+    vf_array2 = vf_array1
     plt.plot(x_ax, vf_array2[:, 0])
     plt.plot(x_ax, vf_array2[:, 1])
     plt.plot(x_ax, vf_array2[:, 2])
     plt.plot(x_ax, vf_array2[:, 3])
     plt.plot(x_ax, vf_array2[:, 4])
     plt.plot(x_ax, vf_array2[:, 5])
-    plt.ylim(-40, 40)
+    # plt.ylim(-1e31, 1e31)
+    # plt.yscale('symlog')
     # pdb.set_trace()
+    plt.show()
+
+
+def read_arr(fname):
+    with open(fname) as f:
+        lines = f.read()
+    vf_array = np.array(pat.findall(lines)).astype(np.float)
+    # pdb.set_trace()
+    return np.mean(vf_array, axis=1)
+
+
+def graph_plot2():
+    vf_array00 = read_arr('./exp2_0_0.txt')
+    vf_array02 = read_arr('./exp2_0_2.txt')
+    vf_array04 = read_arr('./exp2_0_4.txt')
+    vf_array06 = read_arr('./exp2_0_6.txt')
+    vf_array08 = read_arr('./exp2_0_8.txt')
+    vf_array10 = read_arr('./exp2_1_0.txt')
+
+    x_ax = np.arange(vf_array00.shape[0])
+
+    plt.plot(x_ax, vf_array00, label='lambda=0')
+    plt.plot(x_ax, vf_array02, label='lambda=0.2')
+    plt.plot(x_ax, vf_array04, label='lambda=0.4')
+    plt.plot(x_ax, vf_array06, label='lambda=0.6')
+    plt.plot(x_ax, vf_array08, label='lambda=0.8')
+    plt.plot(x_ax, vf_array10, label='lambda=1')
+    # plt.ylim(-1e31, 1e31)
+    # plt.yscale('symlog')
+    plt.legend(bbox_to_anchor=(.85, 1), loc=2, borderaxespad=0.)
     plt.show()
 
 
@@ -202,12 +241,18 @@ if __name__ == '__main__':
     # weights = np.array([-1.2, 1, 14, 1, 1, 10, 1])
     # weights = np.array([0, 0, 0, 0, 0, 0, 0])
     N = 1000000
+    pat = re.compile(r'([-\d.+e]*)\s([-\d.+e]*)\s([-\d.+e]*)\s([-\d.+e]*)\s([-\d.+e]*)\s([-\d.+e]*)\n')
     # N = 5000
-    lamb = 0
-    # lamb = 0.2
+    # lamb = 0
+    lamb = 0.2
+    # lamb = 0.4
+    # lamb = 0.6
+    # lamb = 0.8
+    # lamb = 1
     mdp_inst = mdp_baird(weights)
     td_0 = td_lambda(mdp_inst, N, lamb)
     # td_0.train_model()
     # td_0.train_model1()
     # td_0.train_model_exp1()     #
     # td_0.train_model2()
+    #
